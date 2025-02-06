c302806f2c003531735d650606f31221656b8862
Fernando Souza <fernandofvsouza@icloud.com>
2025-02-06 13:05:32 +0000

    uptade animations

---
+++ Ilha de sao miguel/html/script.js
@@ -0,0 +1,27 @@
+
+
+window.sr = ScrollReveal({ reset: true  });
+
+sr.reveal('.about', {duration: 500 });
+
+sr.reveal('.about-text', {duration: 1000 });
+
+sr.reveal('.food-title', {duration: 500 });
+
+sr.reveal('.imgs', {duration: 1000 });
+
+sr.reveal('.food-description', {duration: 2000 });
+
+sr.reveal('.tourism-title', {duration: 1000 });
+
+sr.reveal('.first-tourism', {rotate: { x:0, y:100, z:0}, duration: 2000 });
+
+sr.reveal('.second-tourism', {rotate: { x:0, y:100, z:0}, duration: 2000 });
+
+sr.reveal('.third-tourism', {rotate: { x:0, y:100, z:0},duration: 2000 });
+
+sr.reveal('.fourth-tourism', {rotate: { x:0, y:100, z:0},duration: 2000 });
+
+sr.reveal('.loc', {duration: 2000 });
+
+sr.reveal('.text-location', {duration: 2000 });
